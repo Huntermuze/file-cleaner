e9563420ef5f7c8c7b59da9d355836a5ee6c782b
huntermuze <mbyamen@gmail.com>
2022-09-09 18:30:27 +1000

    add strict ordering for task1.sh

--- task_1/task1.sh
+++ task_1/task1.sh
@@ -3,5 +3,5 @@
 echo "Enter the dirty file to clean: "
 read -r file_name
 # If we want to sort based shuffle the output, simply pipe the output to the shuf command.
-time (grep -E "^[a-z]{3,15}$" "$file_name" | uniq | sort -k1.3 > "../data/clean_bash.txt")
+time (grep -E "^[a-z]{3,15}$" "$file_name" | uniq -u | sort -k1.3 > "../data/clean_bash.txt")
 echo "Successfully cleansed dirty file. Check the current directory for the cleaned file entitled \"clean_bash.txt\"."
