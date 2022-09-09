bdb0c2d98ce69af64981c1b77ac4669942d64ea0
huntermuze <mbyamen@gmail.com>
2022-09-09 18:17:03 +1000

    fix task1.sh, so that it outputs the correct amt of lines

--- task_1/task1.sh
+++ task_1/task1.sh
@@ -2,6 +2,6 @@
 
 echo "Enter the dirty file to clean: "
 read -r file_name
-# If we want to sort based on 3rd character onwards, simply use the flag "-k1.3" for sort command.
-time (grep -E "^[a-z]{3,15}$" "$file_name" | sort -u -k1.3 > "../data/clean_bash.txt")
+# If we want to sort based shuffle the output, simply pipe the output to the shuf command.
+time (grep -E "^[a-z]{3,15}$" "$file_name" | uniq | sort -k1.3 > "../data/clean_bash.txt")
 echo "Successfully cleansed dirty file. Check the current directory for the cleaned file entitled \"clean_bash.txt\"."
