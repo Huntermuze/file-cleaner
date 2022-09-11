9cea918a7885763eb0d902d25e93e1fc667c5463
huntermuze <mbyamen@gmail.com>
2022-09-11 19:43:53 +1000

    Finalise task 5 and fix bugs

--- task_5/task5.cpp
+++ task_5/task5.cpp
@@ -5,20 +5,19 @@
 #include <sys/types.h>
 #include <cerrno>
 #include <fcntl.h>
-#include <iomanip>
 #include <pthread.h>
-#include "../utilities/function_timer.h"
 #include "../task_1/word_filter.h"
-#include "../utilities/utils.h"
 
-#define cursup    "\033[A"
+#define cursup "\033[A"
 
-bool refresh_data = true;
-int word_length_counts[13] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-std::vector<int *> reduce_file_descriptors(13);
 
+// THE ORIGINAL PLAN:
 // MAP opens the fifos for reading, and sends the word to the appropriate reduce thread, which the reduce thread
 // then increments the counter for that length n word and another thread displays the word_length_counts on screen real-time.
+// This was SCRAPPED because it is very inefficient, and takes a lot more unnecessary code to get working, as opposed to
+// the current way.
+bool refresh_data = true;
+int word_length_counts[13] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
 void *map5(void *arg) {
     auto *path = static_cast<std::string *>(arg);
@@ -29,7 +28,6 @@ void *map5(void *arg) {
         ++word_length_counts[curr_word->length() - MIN_WORD_LENGTH];
     }
 
-    refresh_data = false;
     close(fd);
 //    delete curr_word;
 
@@ -38,27 +36,32 @@ void *map5(void *arg) {
 
 void *display_count(void *) {
     printf("LIST DATA: (COUNT), (RATIO)\n");
-    unsigned long sum = 0;
+    unsigned long sum;
 
     while (refresh_data) {
-//        //FIXME
-        usleep(100000);
+        int point_zero_five_of_second = 50 * 1000;
+        usleep(point_zero_five_of_second);
         sum = 0;
 
         for (int i = MIN_WORD_LENGTH; i <= MAX_WORD_LENGTH; ++i) {
             sum += word_length_counts[i - MIN_WORD_LENGTH];
         }
 
-//        for (int i = MIN_WORD_LENGTH; i <= MAX_WORD_LENGTH; ++i) {
-//            int count = word_length_counts[i - MIN_WORD_LENGTH];
-//            double ratio = (sum == 0) ? 0.0 : ((double) count / (double) sum) * 100;
-//            printf("Length %d list data: %d, %.2f%%\n", i, count, ratio);
-//        }
-//
-//        for (int i = MIN_WORD_LENGTH; i <= MAX_WORD_LENGTH; ++i) {
-//            printf("%s", cursup);
-//        }
-//        printf("\r");
+        for (int i = MIN_WORD_LENGTH; i <= MAX_WORD_LENGTH; ++i) {
+            int count = word_length_counts[i - MIN_WORD_LENGTH];
+            double ratio = (sum == 0) ? 0.0 : ((double) count / (double) sum) * 100;
+            printf("Length %d list data: %d, %.2f%%\n", i, count, ratio);
+        }
+
+        for (int i = MIN_WORD_LENGTH; i <= MAX_WORD_LENGTH; ++i) {
+            printf("%s", cursup);
+        }
+        printf("\r");
+    }
+
+    // Bring cursor back to where it should be.
+    for (int i = MIN_WORD_LENGTH; i <= MAX_WORD_LENGTH; ++i) {
+        printf("\n");
     }
 
     return EXIT_SUCCESS;
@@ -67,9 +70,7 @@ void *display_count(void *) {
 int run_task5() {
     auto *fifo_path = new std::string("task_5_fifo");
     pthread_t map_thread;
-    pthread_t reduce_thread;
     pthread_t display_count_thread;
-
     int status = mkfifo(fifo_path->c_str(), 0777);
 
     if (status == -1 && errno != EEXIST) {
@@ -85,42 +86,40 @@ int run_task5() {
     }
 
     if (pthread_create(&display_count_thread, nullptr, &display_count, nullptr) != 0) {
-        fprintf(stderr, "Failed to create reduce thread.\n");
+        fprintf(stderr, "Failed to create display_count thread.\n");
         return 5;
     }
 
     char current_word[16];
     std::string to_write;
     int fd = open(fifo_path->c_str(), O_WRONLY);
-
     while (scanf("%15s", current_word) != EOF) {
         to_write = current_word;
         write(fd, &to_write, sizeof(to_write));
     }
-
     close(fd);
+    // We will wait half a second, so that the display_count thread can print the most updated version of the word_length_counts
+    // list (ALWAYS), otherwise, depending on the sleep amount in the display_count thread, it may terminate before the most updated
+    // sizes are printed, making the output per run sometimes different.
+    usleep(500 * 1000);
+    refresh_data = false;
+
+    if (pthread_join(display_count_thread, nullptr) != 0) {
+        fprintf(stderr, "Failed to join display_count thread.\n");
+        return 9;
+    }
 
     // Join map thread at the end, ONCE it is finished.
     if (pthread_join(map_thread, nullptr) != 0) {
         fprintf(stderr, "Failed to join map thread.\n");
         return 3;
     }
-
-    if (pthread_join(display_count_thread, nullptr) != 0) {
-        fprintf(stderr, "Failed to join map thread.\n");
-        return 9;
-    }
-
+    printf("\n");
     unlink(fifo_path->c_str());
     delete fifo_path;
     return EXIT_SUCCESS;
 }
 
-
 int main() {
     run_task5();
 }
-
-// TODO map literally passes it to the right reduce thread, and reduce increments a counter for that length_n_list and
-//  (no need to store the words in the lists, just increment the counter) and then display the numbers, in real-time
-//  to stdout on terminal.
